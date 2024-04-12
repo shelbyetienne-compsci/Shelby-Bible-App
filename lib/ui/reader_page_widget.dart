f35e57f62074eab49adc6605e9281db57e8ceb0f
Shelby Etienne <shelby.etienne@life.church>
2024-04-11 20:11:19 -0500

    my own top bar

--- lib/ui/reader_page_widget.dart
+++ lib/ui/reader_page_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:se_bible_project/controllers/chapter_controller.dart';
@@ -28,18 +29,43 @@ class _BibleReaderPageState extends ConsumerState<BibleReaderPageWidget> {
   Widget build(BuildContext context) {
     final chapterState =
         ref.watch(chapterStateNotifierProvider(Testaments.allBooks));
-    final chapterController = ref.read(chapterStateNotifierProvider(Testaments.allBooks).notifier);
+    final chapterController =
+        ref.read(chapterStateNotifierProvider(Testaments.allBooks).notifier);
     return Scaffold(
-      appBar: AppBar(
-        title: const BookButtonWidget(),
-        automaticallyImplyLeading: false,
+      body: Column(
+        children: [
+          const TopBarWidget(),
+          Expanded(
+            child: GestureDetector(
+              onHorizontalDragEnd: (details) async {
+                await chapterController.swipes(details);
+              },
+              child: BibleReaderListWidget(
+                chapterState: chapterState,
+              ),
+            ),
+          ),
+        ],
       ),
-      body: GestureDetector(
-        onHorizontalDragEnd: (details) async {
-          await chapterController.swipes(details);
-        },
-        child: BibleReaderListWidget(
-          chapterState: chapterState,
+    );
+  }
+}
+
+class TopBarWidget extends StatelessWidget {
+  const TopBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top),
+      child: const SizedBox(
+        height: 56,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BookButtonWidget(),
+            // NOTES coming soon
+          ],
         ),
       ),
     );

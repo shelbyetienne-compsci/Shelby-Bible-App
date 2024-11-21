b09e1eb2e05bb1b5c860480ac1a3b6aa7dad822a
Shelby Etienne <67346746+shelbyetienne-compsci@users.noreply.github.com>
2024-11-20 19:07:14 -0500

    offline kjv functionality

--- lib/databases/offline/download_bible.dart
+++ lib/databases/offline/download_bible.dart
@@ -7,7 +7,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class OfflineDatabaseManager {
-  static bool kjvIsDownloaded = false;
   static const String _dbName = 'offline-bible-kjv.db';
 
   static Future<Database> initOfflineDb() async {
@@ -31,22 +30,45 @@ class OfflineDatabaseManager {
         // Write the file to the device
         final file = File(dbPath);
         final savedFile = await file.writeAsBytes(response.bodyBytes);
-        kjvIsDownloaded = true;
         return savedFile;
       } else {
         throw Exception('Failed to download database');
       }
     } catch (e) {
-      kjvIsDownloaded = false;
       rethrow;
     }
   }
 
-  static Future<void> isDatabaseDownloaded() async {
-    final dir = await getApplicationDocumentsDirectory();  // Persistent location
+  static Stream<bool> isDatabaseDownloaded() async* {
+    final dir = await getApplicationDocumentsDirectory(); // Persistent location
     final dbPath = join(dir.path, _dbName);
-    kjvIsDownloaded = await File(dbPath).exists();
+    final file = File(dbPath);
+
+    bool exists = await file.exists();
+    yield exists;
+
+    if (Platform.isAndroid) {
+      await for (var event in file.parent.watch()) {
+        bool newExists = await file.exists();
+        if (newExists != exists) {
+          exists = newExists;
+          yield exists;
+        }
+      }
+    } else {
+      final controller = StreamController<bool>.broadcast();
+      bool fileExists = await file.exists();
+      if(!fileExists){
+        Timer.periodic(const Duration(seconds: 1), (timer) async {
+          bool newExists = await file.exists();
+          if (newExists != exists) {
+            exists = newExists;
+            controller.add(exists);
+          }
+        });
+      }
+      controller.add(fileExists);
+      yield* controller.stream;
+    }
   }
 }
-
-
